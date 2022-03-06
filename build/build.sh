38c84e58a8cc9afd5f9800e41ce383a90590c10f
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2022-03-06 11:30:42 +0000

    stuff

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,7 @@
 cd build
 mkdir temp
 cp ../main.lua temp/main.lua
+cp ../conf.lua temp/conf.lua
 cp -r ../code temp/code
 cp -r ../libs temp/libs
 cp -r ../assets temp/assets
@@ -13,11 +14,14 @@ mv quest.love ..
 cd ..
 rm -rf temp
 
-cp -r love-11.4-win64 game
-cat game/love.exe quest.love > game/quest.exe
-rm game/love.exe
-cd game
-zip -9 -r quest.zip .
-mv quest.zip ..
-cd ..
-rm -rf game
+# Windows Executable
+cp -r love-11.4-win64 quest
+cat quest/love.exe quest.love > quest/quest.exe
+rm quest/love.exe
+rm quest/lovec.exe
+zip -9 -r quest.zip quest
+rm -rf quest
+
+# MacOS App
+cp quest.love quest.app/Contents/Resources/
+
---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    t.window.title = "The Quest by Eduardo Espadeiro"         -- The window title (string)
+    t.window.icon = "assets/Tiles/tile_0000.png"
+end
